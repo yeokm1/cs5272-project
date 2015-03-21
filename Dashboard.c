b1ae4b17fb88dbbcaba0864dfa7b7f87c0f64bf8
Yeo Kheng Meng <yeokm1@gmail.com>
2015-03-21 14:03:09 +0800

    get value via mailbox

--- Dashboard.c
+++ Dashboard.c
@@ -13,15 +13,68 @@
 #include <91x_lib.H>
 #include "LCD.h"
 #include <stdlib.h>
+#include <cstdio>
 
-extern __irq void ADC_IRQ_Handler (void); /* ADC  interrupt routine           */
-extern unsigned char AD_in_progress;      /* AD conversion in progress flag  */
-extern short Potentiometer;
+#include <91x_lib.h>
+#include <RTL.h>
+#include <stdlib.h> 
 
+os_mbx_declare (mailbox_slidesensor, 20); 
+os_mbx_declare (mailbox_potsensor, 20); 
 
 unsigned char A0,A1; //A0 - Button 3.5, A1 - Button 3.6
 unsigned char B0 = 0,B1 = 0,B2 = 0,B3 = 0,B4 = 0,B5 = 0,B6 = 0,B7 = 0; //B0-B7 represent LED's 0 through 7
+unsigned char AD_in_progress;           /* AD conversion in progress flag     */
 
+OS_TID id_task_adc_send;
+OS_TID id_task_adc_recv;
+
+void printMessage(char * buff){
+		LCD_cls();
+		LCD_puts((unsigned char *) buff);
+}
+
+
+void printNumber(int number){
+	  char buff[20];
+		sprintf(buff, "%d", number);
+		printMessage(buff);
+}
+
+
+void initMailBoxes(){
+	os_mbx_init (&mailbox_slidesensor, sizeof (mailbox_slidesensor)); 	
+}
+
+
+
+
+
+__irq void ADC_IRQ_Handler (void) {     /* AD converter interrupt routine     */
+
+	int potValue = ADC->DR0 & 0x03FF;    /* AD value for global usage (10 bit) */	
+	int slideValue = ADC->DR1 & 0x03FF;          /* AD value for global usage (10 bit) */	//dr4.1
+	
+	int * slidePointer = malloc(sizeof(int));
+	int * potPointer = malloc(sizeof(int));
+	
+	
+	* slidePointer = slideValue;
+	* potPointer = potValue;
+	
+
+	os_mbx_send (&mailbox_slidesensor, slidePointer, 0xFFFF);  
+	os_mbx_send (&mailbox_potsensor, potPointer, 0xFFFF);  
+
+	
+  ADC->CR &= 0xFFFE;                    /* Clear STR bit (Start Conversion)   */
+  ADC->CR &= 0x7FFF;                    /* Clear End of Conversion flag       */
+
+  VIC0->VAR = 0;                        /* Acknowledge Interrupt              */  
+  VIC1->VAR = 0;
+
+  AD_in_progress = 0;                   /* Clear flag, as AD conv finished    */
+}
 
 
 //Function to read input
@@ -34,15 +87,6 @@ void read_buttons()
 
 }
 
-void start_ADC ( )
-{
-	if (!AD_in_progress)  
-	{             						    /* If conversion not in progress      */
-        AD_in_progress = 1;                 /* Flag that AD conversion is started */
-        ADC->CR |= 0x0423;                  /* Set STR bit (Start Conversion)     */
-    }
-	//Now the interrupt will be called when conversion ends
-}
 
 //Function to write to led
 void write_led()
@@ -67,15 +111,60 @@ void write_led()
 __task void ADC_Con(void){
   // timing
 	const unsigned int period = 100;
+
 	os_itv_set(period);	
-		for(;;){ 
+	for(;;){ 
 		os_itv_wait();
 		/* Do actions below */
-		start_ADC();
+		if (!AD_in_progress){             						    /* If conversion not in progress      */
+        AD_in_progress = 1;                 /* Flag that AD conversion is started */
+        ADC->CR |= 0x0423;                  /* Set STR bit (Start Conversion)     */
 		}
+	//Now the interrupt will be called when conversion ends
+			
+
+					
+	}
 }	 // End ADC_Con(void)
 
 
+__task void ADC_Recv(void){
+		void * slideMsg;
+		void * potMsg;
+	
+		int slideValue;
+		int potValue;
+	
+		char buff[20];
+		while(1){
+			
+			os_mbx_wait (&mailbox_slidesensor, &slideMsg, 0xffff); 
+	
+			slideValue =  * ((int * ) slideMsg);
+			free(slideMsg);
+			
+		  os_mbx_wait (&mailbox_potsensor, &potMsg, 0xffff); 
+			
+			potValue =  * ((int * ) potMsg);
+			
+			free(potMsg);
+			
+
+			sprintf(buff, "%d, %d", slideValue, potValue);
+			
+			printMessage(buff);
+			
+
+
+		}
+		
+	
+	
+	
+	
+	
+
+}
 
 
 
@@ -129,7 +218,10 @@ __task void init (void) {
   LCD_cur_off(); //Remove LCD cursor
   LCD_cls(); //Clearing LCD screen
 
- 
+	initMailBoxes();
+
+  id_task_adc_send = os_tsk_create(ADC_Con,1);
+	id_task_adc_recv = os_tsk_create(ADC_Recv,2);
   
   os_tsk_delete_self ();
 }
